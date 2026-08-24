c1c27dfd2587d5f77492a837f0eaf284f02857a5
sanjana-jiwane <sanjanajiwane@gmail.com>
2026-08-24 21:28:20 +0530

    Add files via upload
    Buy and Sell Stock Problem

---
+++ Arrays/BuySellStock.java
@@ -0,0 +1,29 @@
+package Arrays;
+
+public class BuySellStock {
+    public static int maxProfit(int[] prices) {
+        int minimum = prices[0];
+        int maxProfit = 0;
+        for(int i = 1; i<prices.length; i++)
+        {
+            int profit = prices[i] - minimum;
+            if(prices[i] < minimum)
+            {
+                minimum = prices[i];
+            }
+            if(profit > maxProfit)
+            {
+                maxProfit = profit;
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 4, 1, 5, 3, 6};
+
+        int result = maxProfit(prices);
+
+        System.out.println("Maximum Profit: " + result);
+    }
+}

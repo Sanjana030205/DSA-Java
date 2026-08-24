ab78045d8758db10e1adf698a0724d5b146d462e
sanjana-jiwane <sanjanajiwane@gmail.com>
2026-08-24 21:46:55 +0530

    Add files via upload
    Contains Duplicate

---
+++ Arrays/ContainsDuplicate.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for(int i = 0; i<nums.length; i++)
+        {
+            if(set.contains(nums[i]))
+            {
+                return true;
+            }
+            set.add(nums[i]);
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1};
+
+        boolean result = containsDuplicate(nums);
+
+        System.out.println(result);
+    }
+}

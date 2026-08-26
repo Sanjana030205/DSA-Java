482579c22e5aa326f6f9716bf75755d7e1831226
sanjana-jiwane <sanjanajiwane@gmail.com>
2026-08-26 19:40:42 +0530

    Create RunningSumOf1DArray.java

---
+++ Arrays/RunningSumOf1DArray.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class RunningSumOf1DArray {
+    public static int[] runningSum(int[] nums) {
+        for(int i = 1; i<nums.length; i++)
+        {
+            nums[i] = nums[i] + nums[i-1];
+        }
+        return nums;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        int[] result = runningSum(nums);
+        System.out.println(java.util.Arrays.toString(result));
+    }
+}
